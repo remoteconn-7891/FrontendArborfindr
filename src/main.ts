36736795c181ea68229e7291a5ba3039a0e983b1
remoteconn-7891 <coreyjames689@aol.com>
2024-12-26 18:31:56 -0700

    new commit

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import router from './router';
 import App from './App.vue';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.css';
+axios.defaults.baseURL = 'http://localhost:8000';
 
 // Set up axios default base URL using environment variable
 axios.defaults.baseURL = import.meta.env.VITE_APP_API_URL;
