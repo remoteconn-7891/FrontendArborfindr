5024ba7c1adcdfd5142cd7a076588c2cc64038fb
remoteconn-7891 <coreyjames689@aol.com>
2025-02-25 03:00:49 -0700

    no comment

--- src/components/pages/RegisterPage.vue
+++ src/components/pages/RegisterPage.vue
@@ -16,7 +16,7 @@
                   v-model="name"
                 />
                 <div v-if="validationErrors.name" class="flex flex-col">
-                  <small class="text-danger">{{ validationErrors.name[0] }}</small>
+                  <small class="text-danger">{{ validationErrors?.name[0] }}</small>
                 </div>
               </div>
               <div class="mb-3">
@@ -29,7 +29,7 @@
                   v-model="email"
                 />
                 <div v-if="validationErrors.email" class="flex flex-col">
-                  <small class="text-danger">{{ validationErrors.email[0] }}</small>
+                  <small class="text-danger">{{ validationErrors?.email[0] }}</small>
                 </div>
               </div>
               <div class="mb-3">
@@ -42,7 +42,7 @@
                   v-model="password"
                 />
                 <div v-if="validationErrors.password" class="flex flex-col">
-                  <small class="text-danger">{{ validationErrors.password[0] }}</small>
+                  <small class="text-danger">{{ validationErrors?.password[0] }}</small>
                 </div>
               </div>
               <div class="mb-3">
@@ -54,18 +54,12 @@
                   name="confirm_password"
                   v-model="confirmPassword"
                 />
-                <div v-if="validationErrors.confirm_password" class="flex flex-col">
-                  <small class="text-danger">{{ validationErrors.confirm_password[0] }}</small>
-                </div>
               </div>
               <div class="d-grid gap-2">
-                <button
+                 <button
                   :disabled="isSubmitting"
                   type="button"
-                  @click="registerAction"
-                >
-                  Register
-                </button>
+                  @click="registerAction">Register</button>
                 <p class="text-center">
                   Already have an account
                   <a href="#" @click.prevent="redirectToLogin">Sign in</a>
@@ -79,55 +73,14 @@
   </layout-div>
 </template>
 
-<script>
-import axios from 'axios';
 
+<script>
 export default {
   name: 'RegisterPage',
-  data() {
-    return {
-      name: '',
-      email: '',
-      password: '',
-      confirmPassword: '',
-      validationErrors: {},
-      isSubmitting: false,
-    };
-  },
   methods: {
-    async registerAction() {
-      this.isSubmitting = true;
-      this.validationErrors = {};
-
-      try {
-        const response = await axios.post('api/register', {
-          name: this.username,
-          email: this.email,
-          password: this.password,
-          password_confirmation: this.confirmPassword,
-        });
-
-        // Handle successful registration (e.g., redirect to login)
-        console.log('Registration successful:', response.data);
-        this.$router.push('/login'); // Example: redirect to login
-      } catch (error) {
-        if (error.response && error.response.data.errors) {
-          this.validationErrors = error.response.data.errors;
-        } else {
-          console.error('Registration error:', error);
-          // Handle other errors (e.g., display a generic error message)
-        }
-      } finally {
-        this.isSubmitting = false;
-      }
-    },
-    redirectToLogin() {
-      this.$router.push('/login');
+    registerAction() {
+      this.$router.push('/homeowner-dashboard');
     },
   },
-  created() {
-    //The code inside of the created hook was not doing what a registration page should do.
-    //If you want to fetch user data after login you should do that in a component that is loaded after login.
-  }
 };
 </script>
