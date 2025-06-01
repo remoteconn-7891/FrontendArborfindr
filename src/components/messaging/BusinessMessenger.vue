faed2114596223a88f2e4205e8bed7f61db1ba6b
Corey James <142646120+remoteconn-7891@users.noreply.github.com>
2025-06-01 15:14:13 -0600

    Update BusinessMessenger.vue
    changes to BusinessMessenger

--- src/components/messaging/BusinessMessenger.vue
+++ src/components/messaging/BusinessMessenger.vue
@@ -14,5 +14,23 @@ const sendMessage = () => {
     ws.send(JSON.stringify({ username: "User", message: messageValue.value }));
     messageValue.value = "";
 };
+
+const unreadMessages = ref(0);
+
+ws.onmessage = (event) => {
+  const data = JSON.parse(event.data);
+  messages.value.push(data);
+
+  // Increase unread count if the message isn't from the user
+  if (data.username !== "User") {
+    unreadMessages.value++;
+  }
+
+<span v-if="unreadMessages > 0" class="notification-badge">
+  {{ unreadMessages }}
+</span>
+
+};
+
 </script>
 
